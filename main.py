2d6c6db10c96d4042ae583dc0a3181a75937d390
Artem <38584948+goldaev51@users.noreply.github.com>
2022-09-29 00:58:29 +0300

    create parse_cookie logic

--- main.py
+++ main.py
@@ -11,7 +11,20 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    if ';' not in query and len(query) <= 1:
+        return {}
+
+    res = {}
+
+    query_split = query.split(';')
+    for task in query_split:
+        if len(task) == 0 or '=' not in task:
+            continue
+        task_split = task.split('=', 1)
+        task_name = task_split[0]
+        task_value = task_split[1]
+        res[task_name] = task_value
+    return res
 
 
 if __name__ == '__main__':
@@ -19,3 +32,12 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie(';') == {}
+    assert parse_cookie('name=Dima=User;age=28;job=developer;nationality=ukr') == {'name': 'Dima=User', 'age': '28',
+                                                                                   'job': 'developer',
+                                                                                   'nationality': 'ukr'}
+    assert parse_cookie('name=====Dima=User=====;age=28;') == {'name': '====Dima=User=====', 'age': '28'}
+    assert parse_cookie('name=====Dima=User=====;;;;;;;;age=28;') == {'name': '====Dima=User=====', 'age': '28'}
+    assert parse_cookie('name=====Dima=User=====;;;;;;;;age=28;job-developer') == {'name': '====Dima=User=====',
+                                                                                   'age': '28'}
+    assert parse_cookie('name;Dima;') == {}
